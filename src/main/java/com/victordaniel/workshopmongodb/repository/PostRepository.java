ba24eac28dd788a693dfd38905a0a23c5e473d9d
victordanielrn <danielrn14@hotmail.com>
2023-09-21 17:44:00 -0300

    Consulta com varios criterios

--- src/main/java/com/victordaniel/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/victordaniel/workshopmongodb/repository/PostRepository.java
@@ -2,14 +2,20 @@ package com.victordaniel.workshopmongodb.repository;
 
 import com.victordaniel.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository <Post, String> {
 
-
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
     //Para quando pesquisar não ter interferencia se a letra e maiuscula ou minuscula
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/victordaniel/workshopmongodb/services/PostServices.java
+++ src/main/java/com/victordaniel/workshopmongodb/services/PostServices.java
@@ -9,6 +9,8 @@ import com.victordaniel.workshopmongodb.services.exception.ObjectNotFoundExcepti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +24,11 @@ public class PostServices {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
     public List<Post>findByTitle(String text){
-        return postRepository.findByTitleContainingIgnoreCase(text);
+        return postRepository.searchTitle(text);
+    }
 
+    public List<Post> fullSearch (String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
